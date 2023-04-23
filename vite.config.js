ca8a0b672b9c07ad825dadae35c3039ad32fe10b
Sahil B <33016488+S-hil@users.noreply.github.com>
2023-04-23 14:32:42 -0700

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'build', // Change this to the desired output folder name
+//   },
+// })
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'build', // Change this to the desired output folder name
-  },
+  plugins: [react()]
 })
 
